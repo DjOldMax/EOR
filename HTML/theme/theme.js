7dadfd1d15c7a18b35fa4475810972e6f10d5d99
Sergey <62072065+Wildamager@users.noreply.github.com>
2022-07-15 02:40:50 +0700

    Убрал лишние файлы

--- HTML/theme/theme.js
+++
@@ -1,9 +0,0 @@
-var theme =
-[
-	['<IMG SRC=theme/folder.gif> ', '<IMG SRC=theme/folderopen.gif> '],
-	['', ''],
-	['', ''],
-	'<IMG SRC=theme/page.gif> ',
-	['', ''],
-	['&nbsp;&nbsp;&nbsp;&nbsp;', '&nbsp;&nbsp;&nbsp;&nbsp;'],
-];
